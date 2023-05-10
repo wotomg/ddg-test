001f6c6dd44c2900127d01de6325e74b94902fba
wotomg <wotomg@gmail.com>
2023-05-11 00:15:48 +0300

    bugfix2

--- install.sh
+++ install.sh
@@ -20,11 +20,11 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 systemctl start docker.service
 systemctl enable docker.service
 
-if [[ systemctl is-active --quiet docker && systemctl is-enabled --quiet docker ]]; then
-  echo "Docker is runing and activated on system boot"
-  else
-    echo "!!!!!!!!!!!\nSOMETHING WENT WRONG\n!!!!!!!!!!!"
-fi
+# if [[ systemctl is-active --quiet docker && systemctl is-enabled --quiet docker ]]; then
+#   echo "Docker is runing and activated on system boot"
+#   else
+#     echo "!!!!!!!!!!!\nSOMETHING WENT WRONG\n!!!!!!!!!!!"
+# fi
 
 /sbin/usermod -aG docker $1
 
--- start.sh
+++ start.sh
@@ -6,3 +6,4 @@ wget https://github.com/wotomg/ddg-test/archive/main.tar.gz
 tar -zxvf main.tar.gz
 cd ddg-test-main
 su -c 'bash install.sh $username'
+docker-compose up -d
