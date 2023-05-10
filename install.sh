711e2fd38236e4abc86daa7d45aea08c609dd07d
wotomg <wotomg@gmail.com>
2023-05-11 00:09:00 +0300

    bugfix

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ fi
 # INSTALLING DOCKER
 apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl start docker.service
--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 username = $(whoami)
 
 wget https://github.com/wotomg/ddg-test/archive/main.tar.gz
-tar -zxvf ddg-test-main.tar.gz
+tar -zxvf main.tar.gz
 cd ddg-test-main
-sudo bash install.sh $username
+su -c 'bash install.sh $username'
